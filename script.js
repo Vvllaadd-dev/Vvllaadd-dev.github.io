bdeec6b3de9743ca90dbe528f337f0b3bf6ccda2
Vvllaadd-dev <vladislavch008@gmail.com>
2026-02-15 14:53:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let variable1 = 1
+let variable2 = 2
+let variable3 = 3
+console.log(variable1)
+let btn = document.querySelector(".btn1")
+btn.addEventListener("click",function(){
+
+alert("Спасибо за посещение сайта!")
+}
+)
